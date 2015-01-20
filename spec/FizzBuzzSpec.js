5be2ee0724eb1bb32e8b05f926e65aad2cf933ef
bibiana <bcristofol@gmail.com>
2015-01-20 11:42:02 +0000

    knows when a number is divisible by 5

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -13,7 +13,16 @@ describe("FizzBuzz", function() {
     it("is NOT divisible by 3", function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
 
+    it("is divisible by 5", function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it("is NOT divisible by 5", function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
     });
 
   });
