ad5686dfa783aeb031d172c112f261dbee156607
bibiana <bcristofol@gmail.com>
2015-01-20 11:12:00 +0000

    Check that is divisible by three

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,21 @@
+describe("FizzBuzz", function() {
+
+  var fizzbuzz;
+
+  describe("knows when a number", function() {
+
+    it("is divisible by 3", function() {
+      fizzbuzz = new FizzBuzz();
+      // Ruby -> fizzbuzz = FizzBuzz.new
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true); // 'is' is like a ? in Ruby divisible_by_three?
+    });
+
+    it("is NOT divisible by 3", function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+
+    });
+
+  });
+
+});
